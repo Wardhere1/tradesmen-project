1b52b223fb570150f8c21c095e32a9a5af540ec4
Mohamed Khalaf <mohamed.khalaf@infinityworks.com>
2023-01-09 15:03:34 +0000

    empty-files

--- client/app.js
+++ client/app.js
@@ -1,17 +1,1 @@
-const name = "hello";
 
-function shortcut(string) {
-  const vowels = ["a", "e", "i", "o", "u"];
-  for (let v = 0; length = ; v++) {
-    const newWord = string.replace(v, "");
-    console.log(newWord);
-  }
-  return string;
-}
-//vowels.forEach((vowel) => {
-//const word = string.replace(vowel, "");
-//});
-
-shortcut("hello");
-// hey
-//hello.
