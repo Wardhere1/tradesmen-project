83a597907308100d44f1d55ba4bb4ef10dd826d8
wardhere1 <wardhereadan@gmail.com>
2023-01-18 16:18:46 +0000

    server container

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const apiPort = 3003;
+app.listen(apiPort, () => {
+  console.log(`tradesmatch app listening on port ${apiPort}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from tradesmatch');
+});
