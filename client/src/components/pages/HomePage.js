02ae104023b9312157121cf116ec9f04faa19305
Mohamed Khalaf <mohamed.khalaf@infinityworks.com>
2023-02-06 19:09:44 +0000

    video-image

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-
+import videoBg from '../assets/background-video.mp4';
 export const HomePage = () => {
   return (
     <div className="Homepage-container">
-      <img className="Trademanimg" src="https://tradesmenonline.co.uk/wp-content/uploads/home-banner.jpg" />
-      <h1 className=''>
+        <video src= {videoBg} autoPlay loop muted />
+      <h1 className="">
         Need a new boiler? Get a boiler fully installed from £1,700 Choose from our range of boilers from the leading
         manufacturers. We find you vetted local Gas Safe installers Local Heroes 12 month guarantee, backed by British
         Gas Choice of up to 10 years manufacturer’s warranty. Flexible payment options available
