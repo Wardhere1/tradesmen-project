e2ad0deb99c4e16cb5a692d56b4a79341baaec8d
Khalid4555 <knur7546@gmail.com>
2023-02-07 14:08:18 +0000

    styling

--- client/src/components/Quote.js
+++ client/src/components/Quote.js
@@ -14,12 +14,12 @@ export const Quote = () => {
                     <p>Let us know what job needs doing, get an online estimate and then choose a date and time that works for you</p>
                 </div>
                 <div className='quotes'>
-                    <img className= 'quote-image'src="https://assets.localheroes.com/lh-ui/how-it-works/local-heroes-icon-phone-message-blue.svg" alt="Icon of a phone with message" />
+                    <img className= 'quote-image'src="https://assets.localheroes.com/lh-ui/how-it-works/local-heroes-icon-clipboard-blue.svg" alt="Icon of a phone with message" />
                     <h4>We'll connect you to a tradesman</h4>
                     <p>We'll circle our large pool of tradesman to find the best suited tradesperson for you!</p>
                 </div>
                 <div className='quotes'>
-                    <img className= 'quote-image' src="https://assets.localheroes.com/lh-ui/how-it-works/local-heroes-icon-phone-message-blue.svg" alt="Icon of a phone with message" />
+                    <img className= 'quote-image' src="https://assets.localheroes.com/lh-ui/how-it-works/local-heroes-icon-phone-payment-blue.svg" alt="Icon of a phone with message" />
                     <h4>Get a quote</h4>
                     <p>A hero in your local area will get in touch and give you a quote. If you're happy, you can accept the quote or cancel for free</p>
                 </div>
