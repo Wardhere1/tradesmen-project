c4db60e72a8d1c84f3d59f386fba976000fbdf8f
Khalid4555 <knur7546@gmail.com>
2023-02-09 16:21:15 +0000

    styling

--- client/src/components/pages/ServicePage.js
+++ client/src/components/pages/ServicePage.js
@@ -21,7 +21,7 @@ export const ServicePage = () => {
         <div className='service-container'>
             <HowItWorks />
             <ServicesList servicesData={servicesData} />
-            <button className='next-button'>Next</button>
+            <button className='next-button'>NEXT</button>
         </div>
     )
 }
