766d17916fb6528f16651521ece0fdd02905f4c7
Mohamed Khalaf <mohamed.khalaf@infinityworks.com>
2023-03-30 15:59:53 +0100

    Testing

--- cypress/e2e/myjourney.spec.cy.js
+++ cypress/e2e/myjourney.spec.cy.js
@@ -6,7 +6,11 @@ describe('template spec', () => {
     cy.get('.button-homepage-2').contains('SELECT A SERVICE').click()
     cy.location("href").should("eq", "http://localhost:3000/services");
     cy.get('.service-card').contains('Plumber').click()
+    cy.get('.service-card').contains('Masonry').click()
     cy.get('.next-button').click()
     cy.location("pathname").should("eq","/customer-sign-up" )
   });
 });
+
+
+//Test how to see if service card contains the services which are pulled from the database table.
